9657b7c69e66d83ec58beaacba5a720abc273df3
Razan <thamelstreet@gmail.com>
2011-11-29 22:31:11 +0545

    Corrected .gitignore error and added some OpenGL calls

--- HelloOpenGL/OpenGLView.h
+++ HelloOpenGL/OpenGLView.h
@@ -7,7 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#import <OpenGLES/ES2/gl.h>
+#import <OpenGLES/ES2/glext.h>
 
-@interface OpenGLView : UIView
+@interface OpenGLView : UIView {
+    CAEAGLLayer *_eaglLayer;
+    EAGLContext *_context;
+    GLuint _colorRenderBuffer;
+}
 
 @end
--- HelloOpenGL/OpenGLView.m
+++ HelloOpenGL/OpenGLView.m
@@ -19,6 +19,50 @@
     return self;
 }
 
+/* Static Class for CAEAGLLayer Class */
++ (Class) layerClass {
+    return [CAEAGLLayer class];
+}
+
+/* Setting up Layer */
+- (void) setupLayer {
+    _eaglLayer = (CAEAGLLayer *)self.layer;
+    _eaglLayer.opaque = YES;
+}
+
+/* */
+- (void) setupContext {
+    /* We have decided here to use OpenGLAPI 2.0, if device is using 1.0 api, app will terminate */
+    EAGLRenderingAPI api = kEAGLRenderingAPIOpenGLES2;
+    _context = [[EAGLContext alloc] initWithAPI:api];
+    
+    if (!_context) {
+        NSLog(@"Failed to initialize OpenGLES 2.0 context");
+        exit(1);
+    }
+    
+    if (![EAGLContext setCurrentContext:_context]) {
+        NSLog(@"Failed to set current OpenGL context");
+        exit(1);
+    }
+}
+
+/* Setup Render Buffer */
+- (void) setupRenderBuffer {
+    glGenRenderbuffers(1, &_colorRenderBuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, _colorRenderBuffer);
+    [_context renderbufferStorage:GL_RENDERBUFFER fromDrawable:_eaglLayer];
+}
+
+/* Create a frame Buffer */
+- (void)setupFrameBuffer {    
+    GLuint framebuffer;
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, _colorRenderBuffer);
+}
+
+
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.

5569dbf62d8cfd7d4455c52c046244c689bd8e6d
Razan <thamelstreet@gmail.com>
2011-11-29 22:03:04 +0545

    Added OpenGLView Class and updated .gitignore for ignoring .xcodeproj/project.pbxproj

---
+++ HelloOpenGL/OpenGLView.h
@@ -0,0 +1,13 @@
+//
+//  OpenGLView.h
+//  HelloOpenGL
+//
+//  Created by Rajan Maharjan on 11/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpenGLView : UIView
+
+@end
